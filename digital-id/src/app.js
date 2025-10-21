5c98789408fb57dba7acbac2646d083cb1e433f8
MatrixSolutionsNetwork <thematrixsolutions@proton.me>
2025-10-21 22:11:03 +0100

    Update app.js

--- digital-id/src/app.js
+++ digital-id/src/app.js
@@ -1,1 +1,22 @@
+import * as UI from './ui.js'
+import * as Geo from './geo.js'
+import * as Data from './data.js'
+import * as MapMod from './map.js'
 
+export async function start(cfg){
+  UI.initTopbar(cfg.title)
+
+  const world = await Geo.loadWorld()
+  if(!world){ console.error('World polygons failed to load'); return }
+
+  const centroids = Geo.buildCentroids(world)
+
+  const csvText = await Data.fetchCsvOrFallback(cfg.sheetCsv)
+  UI.setUpdated(Data.updatedAt())
+
+  const rows = Data.parseCsv(csvText)
+  const entries = Data.normalize(rows, centroids, cfg, Geo.normalizeCountry)
+
+  const map = MapMod.init()
+  MapMod.draw({ world, entries, centroids, cfg, map })
+}
